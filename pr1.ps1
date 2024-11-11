df110502c5343e25a1191fe9057972b28c96ae0a
Rose Eiche <rose.eiche@voco.ee>
2024-11-11 10:44:51 +0200

    Practice 1 - using += operator

---
+++ pr1.ps1
@@ -0,0 +1,16 @@
+﻿
+# define two arrays
+$a1 = @(1,2,3)
+$a2 = @(4,5,6)
+
+# define empty array
+$a3 = @()
+
+
+# add new values to empty array
+$a3 += $a1[0] + $a2[0]
+$a3 += $a1[1] + $a2[1]
+$a3 += $a1[2] + $a2[2]
+
+# print added values
+$a3
