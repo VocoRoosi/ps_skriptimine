7485ca73bb23876b95910bea2343c7f9356bfe2e
Rose Eiche <rose.eiche@voco.ee>
2024-11-18 11:48:29 +0200

    Practice 8 - Get-Process/Write-Host Notepad running script

---
+++ pr8.ps1
@@ -0,0 +1,6 @@
+﻿
+#Script will display that Notepad is running until you close it, then script stops.
+while(Get-Process Notepad -ErrorAction SilentlyContinue)
+  {
+   Write-Host "Notepad is running"
+  }
