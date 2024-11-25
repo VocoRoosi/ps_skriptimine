39bc63370555bd985fb243f6daec88e71884be40
Rose Eiche <rose.eiche@voco.ee>
2024-11-25 09:27:42 +0200

    Practice 9 - do while loop notepad is running script

---
+++ pr9.ps1
@@ -0,0 +1,17 @@
+﻿# Initialize counter
+$step = 0
+
+# Open 3 instances of Notepad
+Start-Process "notepad.exe"
+Start-Process "notepad.exe"
+Start-Process "notepad.exe"
+
+# Do-while loop to check for running Notepad processes
+do {
+    Write-Host 'Notepad is running'
+    $step++  # Increment the counter
+    Start-Sleep -Seconds 1  # Wait for 1 second
+} while (Get-Process Notepad -ErrorAction SilentlyContinue)
+
+# Display the number of times the message was displayed
+Write-Host $step
