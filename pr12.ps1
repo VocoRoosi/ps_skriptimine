bdacbbfc2ba6bee0a896c3e11759654db9adadfa
Rose Eiche <rose.eiche@voco.ee>
2024-11-25 11:26:10 +0200

    Practice 12 - with different call of parameters

---
+++ pr12.ps1
@@ -0,0 +1,23 @@
+﻿function countstart {
+     $services = Get-Service | Where {$_.Status -like 'Running'}
+     $services.Count
+}
+function countstop {
+     $services = Get-Service | Where {$_.Status -like 'Stopped'}
+     $services.Count
+}
+
+
+function countservice {
+   param (
+         [string]$status
+   )
+   $services= Get-Service | Where {$_.Status -like $status}
+   $services.Count
+}
+
+Write-Host "Total services in Running state = $(countstart)"
+Write-Host "Total services in Stopped state = $(countstop)"
+Write-Host
+Write-Host "Total services in Running state = $(countservice('Running'))"
+Write-Host "Total services in Stopped state = $(countservice -status 'Stopped')"
