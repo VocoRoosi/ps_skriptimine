28fd29f40d69d9be96b5fb490587e5fd58d1b4b9
Rose Eiche <rose.eiche@voco.ee>
2024-11-25 11:06:17 +0200

    Practice 5 part 2 - display total files in the folder and display .csv file size

---
+++ pr5p2.ps1
@@ -0,0 +1,17 @@
+﻿Get-ChildItem -Path "C:\temp\test"
+
+# Show how many number of files are in the folder
+$file = Get-ChildItem -Path "C:\Temp\Test" | where {$_.Name -like "*.csv"} | Select Name,Length
+foreach ($file in $files) {
+$sizeinKB = $file.Length/1KB
+$sizeinMB = $file.Length/1MB
+
+$result = Get-ChildItem -Path C:\Temp\Test\* -Include *.csv | Select Name, Length
+
+$result
+
+Write-Host
+Write-Host "`nFileName  : $($file.Name)"
+Write-Host "Size in KB : "$sizeinKB
+Write-Host "Size in MB : "$sizeinMB
+}
