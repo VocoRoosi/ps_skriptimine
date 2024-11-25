e0d4521c2b9facf9492fb2919e3b69ee3fd7a64e
Rose Eiche <rose.eiche@voco.ee>
2024-11-25 09:44:28 +0200

    Practice 10 - do until loop notepad is running script

---
+++ pr10.ps1
@@ -0,0 +1,19 @@
+﻿# Initialize counter
+$step = 0
+$isNotepadOpen = (Get-Process Notepad -ErrorAction SilentlyContinue)
+
+# Open 3 instances of Notepad
+Start-Process "notepad.exe"
+Start-Process "notepad.exe"
+Start-Process "notepad.exe"
+
+# Do-Until loop to check for running Notepad processes
+do {
+    Write-Host 'Notepad is running'
+    $step++  # Increment the counter
+    $isNotepadOpen = (Get-Process Notepad -ErrorAction SilentlyContinue)
+    Start-Sleep -Seconds 1  # Wait for 1 second
+} until ($isNotepadOpen.Count -eq 0)
+
+# Display the number of times the message was displayed
+Write-Host $step
